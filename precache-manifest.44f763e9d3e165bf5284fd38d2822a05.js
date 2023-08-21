c0d4912a21523709a2cf73ae8b109b03253af0ab
vladporcila <vlad.porcila@humber.ca>
2023-08-21 08:38:13 -0400

    Website 2.0
    Website 2.0

---
+++ precache-manifest.44f763e9d3e165bf5284fd38d2822a05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9bd348ccf52e8131dc23",
+    "url": "/static/css/main.b33e38db.chunk.css"
+  },
+  {
+    "revision": "9bd348ccf52e8131dc23",
+    "url": "/static/js/main.9483215c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7987f41be9e7e9563143",
+    "url": "/static/js/2.b0b39f83.chunk.js"
+  },
+  {
+    "revision": "ac5a15a134d55ebbb97d123ae87daad7",
+    "url": "/index.html"
+  }
+];
